e0c601ca52f0d792d810f2586d26355a4b52e8c8
apierro <antonio.pierro@gmail.com>
2020-02-08 09:38:36 +0100

    making assets served relative to index.html

---
+++ docs/precache-manifest.7f4e4644cb9afed101703847d3f2474c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a15931b7ff643e16f9965b1fa03e0cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "580b7550c77b82e7bae0",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04fe85ee19dffea5cf51",
+    "url": "./static/js/2.5b10fc6f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "./static/js/2.5b10fc6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "580b7550c77b82e7bae0",
+    "url": "./static/js/main.91b5ad93.chunk.js"
+  },
+  {
+    "revision": "4931cf3af60063c5c6f2",
+    "url": "./static/js/runtime-main.fab88f23.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e76fc0e2d870d553e12fec61ce117abb.js"
+  "./precache-manifest.7f4e4644cb9afed101703847d3f2474c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
