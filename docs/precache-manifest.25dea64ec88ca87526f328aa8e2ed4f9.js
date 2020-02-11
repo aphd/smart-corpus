fdfa0c4baeefaa8ef340869574c45de83370a05a
apierro <antonio.pierro@gmail.com>
2020-02-11 11:12:04 +0100

    building

---
+++ docs/precache-manifest.25dea64ec88ca87526f328aa8e2ed4f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d8b82035d955a76e075975ad515382",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3682628313a02b739413",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dab421022e9a4bfdb9c0",
+    "url": "./static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "3682628313a02b739413",
+    "url": "./static/js/2.991143d7.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "./static/js/2.991143d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dab421022e9a4bfdb9c0",
+    "url": "./static/js/main.ff40c774.chunk.js"
+  },
+  {
+    "revision": "4931cf3af60063c5c6f2",
+    "url": "./static/js/runtime-main.fab88f23.js"
+  }
+]);
