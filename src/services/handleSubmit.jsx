4d816d9c5d584321aa5dd84a7d8c7f535fabaa21
apierro <antonio.pierro@gmail.com>
2020-03-03 12:05:46 +0100

    fixing for with query deep copying

--- src/services/handleSubmit.jsx
+++ src/services/handleSubmit.jsx
@@ -3,11 +3,12 @@ export const formOptions = {
     pragma_versions: ["Any", "0.4.25", "0.5.0", "0.5.4"]
 };
 
-export const handleSubmit = function(par) {
-    Object.keys(par).forEach(v => {
-        if (par[v] === "Any") delete par[v];
+export const handleSubmit = function(query) {
+    this.query = Object.assign({}, query);
+    Object.keys(query).forEach(v => {
+        if (this.query[v] === "Any") delete this.query[v];
     });
-    fetch("https://smac.ga/object/" + JSON.stringify(par))
+    fetch("https://smac.ga/object/" + JSON.stringify(this.query))
         .then(res => res.json())
         .then(data => this.setState({ smacs: data }))
         .catch(err => console.log("catch:\n", err));
