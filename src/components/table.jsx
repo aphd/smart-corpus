79333a6a87cb48ded4cc89cba5a31adf6568e409
apierro <antonio.pierro@gmail.com>
2020-03-20 19:17:13 +0100

    adding html-react-parser

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import parse from "html-react-parser";
 
 const RenderRow = props => {
     return props.keys.map((key, index) => {
         let value = props.data[key];
-        if (props.data[key].length) {
-            value = `${value.slice(0, 10)}...`;
+        if (props.data[key].length > 10) {
+            value = parse(`<a href="#">${value.slice(0, 15)}...</a>`);
         }
         return <td key={index}>{value}</td>;
     });
